7ab655f4fc0b751184f9657fe6ec31480685190d
tkgstrator <nasawake.am@gmail.com>
2021-02-07 12:36:46 +0900

    Add getPlayerId function

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SplatNet2",
+    platforms:  [
+        .iOS(.v11)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -13,14 +16,15 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.4.1"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SplatNet2",
-            dependencies: []),
+            dependencies: ["Alamofire", "SwiftyJSON"]),
         .testTarget(
             name: "SplatNet2Tests",
             dependencies: ["SplatNet2"]),
